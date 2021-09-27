d96d21524d8948540bbb4f2051f1440d12d3d708
yenna23 <yennasopia@student.telkomuniversity.ac.id>
2021-09-27 15:20:05 +0700

    Biodata.java

---
+++ Praktikum 2/Biodata.java
@@ -0,0 +1,8 @@
+public class Biodata {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama : Yenna");
+		System.out.println("NIM  : 6706213023");
+		System.out.println("Kelas: D3RPLA 45-03");
+	}
+}
